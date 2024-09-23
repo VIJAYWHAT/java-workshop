8d87dac3fcd28b204b8546193481a09b3c59af83
VijayWhat <vijayrv042@gmail.com>
2024-09-23 21:31:18 +0530

    changes

---
+++ basics/student.java
@@ -0,0 +1,17 @@
+public class student {
+    private int id;
+    private String name;
+    public void setId(int aId) {
+        id = aId;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+    public String getName() {
+        return name;
+    }
+}
