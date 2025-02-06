f6eecf03f7a15e5ed38c561acfb2e9a8f1a82c46
VijayWhat <vijayrv042@gmail.com>
2025-02-06 16:03:05 +0530

    comparison method added

--- java-class/operators.java
+++ java-class/operators.java
@@ -24,6 +24,27 @@ public class operators {
 
     }
 
+    public void comparison() {
+        if (a > b) {
+            System.out.println("a is greater than b");
+        }
+
+        if (a < b) {
+            System.out.println("a is less than b");
+        }
+
+        if (a == b) {
+            System.out.println("They are both equal");
+        }
+
+        // > greater than
+        // < less than
+        // == equal condition
+        // > == greater than or equal to
+        // <== less than or equal to
+
+    }
+
     public static void main(String[] args) {
         operators obj = new operators();
         obj.arithmetic();
