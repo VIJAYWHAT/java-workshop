eaa727a3a72c3e3cbb91b21ccf1643946d37eb6a
VijayWhat <vijayrv042@gmail.com>
2025-02-11 16:03:05 +0530

    Basic array program added

--- java-class/myArray.java
+++ java-class/myArray.java
@@ -2,10 +2,24 @@ public class myArray {
     public static void main(String[] args) {
         
         myArray obj = new myArray();
-        obj.nameSearch();
+        obj.basicArray();
 
-        
     }
+    
+    private void basicArray(){
+        int[] array = new int[5];
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+        array[3] = 40;
+        array[4] = 50;
+        System.out.println("Basic Array");
+        System.out.println("Array length is : " + array.length);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
     private void nameSearch() {
         String [] names = {"John", "Paul", "George", "Ringo"};
         String searchName = "Ringo";
