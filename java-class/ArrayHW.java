d603481aa0cb656300ef4a960d705a7f75f9b909
VijayWhat <vijayrv042@gmail.com>
2025-02-15 16:03:05 +0530

    second reverse array program added

--- java-class/ArrayHW.java
+++ java-class/ArrayHW.java
@@ -1,20 +1,9 @@
 public class ArrayHW {
 
     public static void main(String[] arg) {
-        // ArrayHW arrayHW = new ArrayHW();
-        // arrayHW.arraySum();
-        int[] arr = {1,2,3,4,5};
-        System.out.println();
-        int[] rev = new int[5];
-        int index = 0;
-        for(int i = arr.length-1; i >= 0; i--) {
-        rev[index] = arr[i];
-            index++;
-        }
-        System.out.print("Reversed array is : ");
-        for(int a : rev) {
-            System.out.print(a + " ");
-        }
+        ArrayHW arrayHW = new ArrayHW();
+        arrayHW.reverseArray2();
+        
     }
 
     private void arraySum(){
@@ -36,5 +25,44 @@ public class ArrayHW {
             }
         }
         System.out.println("Maximum value in the array is : " + max);
+    }
+
+    private void reverseArray() {
+        int[] array = {1, 2, 3, 4, 5};
+        int[] array2 = new int[array.length];
+
+        System.out.print("Original array: ");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        for (int i = 0; i < array.length; i++) {
+            array2[i] = array[array.length - 1 - i];
+        }
+
+        System.out.print("\nReversed array: ");
+        for (int num : array2) {
+            System.out.print(num + " ");
+        }
+    }
+    
+    private void reverseArray2() {
+        int[] array = {1, 2, 3, 4, 5};
+        int start = 0;
+        int end = array.length - 1;
+        System.out.print("Original array: ");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        while (start < end) {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
+        }
+        System.out.print("\nReversed array: ");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
     }
 }
