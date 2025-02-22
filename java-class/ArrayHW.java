6fdd289f1df29c62b8c41d7d44c7c6db55161617
VijayWhat <vijayrv042@gmail.com>
2025-02-22 16:13:17 +0530

    find the max value in array"

--- java-class/ArrayHW.java
+++ java-class/ArrayHW.java
@@ -1,7 +1,8 @@
 public class ArrayHW {
     public static void main(String[] args) {
         ArrayHW obj = new ArrayHW();
-        obj.arraySum();
+        obj.maxValue();
+
     }
 
     private void arraySum(){
@@ -12,5 +13,16 @@ public class ArrayHW {
             sum += a;
         }
         System.out.println("Sum of the array is : " + sum);
+    }
+
+    private void maxValue() {
+        int[] arr = {1, 2, 7, 4, 5};
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+            max = arr[i];
+            }
+        }
+        System.out.println("Maximum value in the array is : " + max);
     }
 }
